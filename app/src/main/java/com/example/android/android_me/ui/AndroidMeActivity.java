0d2696f8e3034cd28d9c8b3c690d316e35557dd4
waithethtetoo <wtho.waithet@gmail.com>
2020-06-24 12:06:42 +0630

    communicate between activities

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -1,21 +1,22 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,45 +28,51 @@ import com.example.android.android_me.data.AndroidImageAssets;
 public class AndroidMeActivity extends AppCompatActivity {
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_android_me);
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_android_me);
 
-        // Only create new fragments when there is no previously saved state
-        if(savedInstanceState == null) {
+      // Only create new fragments when there is no previously saved state
+      if (savedInstanceState == null) {
 
-            // TODO (5) Retrieve list index values that were sent through an intent; use them to display the desired Android-Me body part image
-                // Use setListindex(int index) to set the list index for all BodyPartFragments
+         // TODO (5) Retrieve list index values that were sent through an intent; use them to display the desired Android-Me body part image
+         // Use setListindex(int index) to set the list index for all BodyPartFragments
+         // Create a new head BodyPartFragment
+         BodyPartFragment headFragment = new BodyPartFragment();
 
-            // Create a new head BodyPartFragment
-            BodyPartFragment headFragment = new BodyPartFragment();
+         // Set the list of image id's for the head fragment and set the position to the second image in the list
+         headFragment.setImageIds(AndroidImageAssets.getHeads());
+         int headIndex = getIntent().getIntExtra("headIndex", 0);
+         headFragment.setListIndex(headIndex);
 
-            // Set the list of image id's for the head fragment and set the position to the second image in the list
-            headFragment.setImageIds(AndroidImageAssets.getHeads());
-            headFragment.setListIndex(1);
+         // Add the fragment to its container using a FragmentManager and a Transaction
+         FragmentManager fragmentManager = getSupportFragmentManager();
 
-            // Add the fragment to its container using a FragmentManager and a Transaction
-            FragmentManager fragmentManager = getSupportFragmentManager();
+         fragmentManager.beginTransaction()
+                 .add(R.id.head_container, headFragment)
+                 .commit();
 
-            fragmentManager.beginTransaction()
-                    .add(R.id.head_container, headFragment)
-                    .commit();
+         // Create and display the body and leg BodyPartFragments
 
-            // Create and display the body and leg BodyPartFragments
+         BodyPartFragment bodyFragment = new BodyPartFragment();
+         bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+         int bodyIndex = getIntent().getIntExtra("bodyIndex", 1);
+         bodyFragment.setListIndex(bodyIndex);
 
-            BodyPartFragment bodyFragment = new BodyPartFragment();
-            bodyFragment.setImageIds(AndroidImageAssets.getBodies());
-            fragmentManager.beginTransaction()
-                    .add(R.id.body_container, bodyFragment)
-                    .commit();
+         fragmentManager.beginTransaction()
+                 .add(R.id.body_container, bodyFragment)
+                 .commit();
 
-            BodyPartFragment legFragment = new BodyPartFragment();
-            legFragment.setImageIds(AndroidImageAssets.getLegs());
-            fragmentManager.beginTransaction()
-                    .add(R.id.leg_container, legFragment)
-                    .commit();
-        }
+         BodyPartFragment legFragment = new BodyPartFragment();
+         legFragment.setImageIds(AndroidImageAssets.getLegs());
+         int legIndex = getIntent().getIntExtra("legIndex", 2);
+         legFragment.setListIndex(legIndex);
 
-    }
+         fragmentManager.beginTransaction()
+                 .add(R.id.leg_container, legFragment)
+                 .commit();
+      }
+
+   }
 }
